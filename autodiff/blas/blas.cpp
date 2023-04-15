4143fa42ac25779a071bf948a957910028a35c51
jay1999ke <jay1999ke@gmail.com>
2023-04-15 03:29:47 -0400

    Add gpu_add

--- autodiff/blas/blas.cpp
+++ autodiff/blas/blas.cpp
@@ -18,22 +18,18 @@ py::array_t<float> cpu_add(py::array_t<float> a, py::array_t<float> b) {
     int dim1 = a_info.shape[0];
     int dim2 = a_info.shape[1];
 
-    auto result = py::array(py::buffer_info(
-        nullptr,       /* Pointer to data (nullptr -> ask NumPy to allocate!) */
-        sizeof(float), /* Size of one item */
-        py::format_descriptor<float>::value, /* Buffer format */
-        a_info.ndim,                         /* How many dimensions? */
-        {dim1, dim2},   /* Number of elements for each dimension */
-        {sizeof(float), sizeof(float)} /* Strides for each dimension */
-        ));
-
-    py::buffer_info result_info = result.request();
-    auto res_ptr = static_cast<float *>(result_info.ptr);
+    float *res_ptr = new float[dim1 * dim2];
 
     for (int i = 0; i < dim1; i++) {
         for (int j = 0; j < dim2; j++) {
             res_ptr[i * dim1 + j] = a_ptr[i * dim1 + j] + b_ptr[i * dim1 + j];
         }
     }
-    return result;
+
+    return py::array_t<float>(
+        {dim1, dim2}, // shape
+        {sizeof(float) * dim1,
+         sizeof(float)}, // C-style contiguous strides for double
+        res_ptr          // the data pointer
+        );               
 }
--- autodiff/core/tensor.py
+++ autodiff/core/tensor.py
@@ -4,9 +4,10 @@ import numpy as np
 arr2 = np.linspace(1.0,9,9).astype(np.float32).reshape(3,3)
 arr1 = np.linspace(1.0,9,9).astype(np.float32).reshape(3,3)
 
-arr3 = blas.cpu_add(arr1,arr2)
-
-print(arr3)
+print("1  ", arr1)
+print("2  ", arr2)
+print("cpu", blas.cpu_add(arr1, arr2))
+print("gpu", blas.gpu_add(arr1, arr2))
 
 class Tensor(object):
 
