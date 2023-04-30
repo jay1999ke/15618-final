d35f204773f1b2af6e76e4200f2ba0cd89396d87
jay1999ke <jay1999ke@gmail.com>
2023-04-30 05:20:16 -0400

    Make build compatible with ghc cluster

--- autodiff/build.sh
+++ autodiff/build.sh
@@ -1,11 +1,18 @@
 git submodule init
 git submodule update
+python3 -m pip install pybind11-stubgen
 cd build
 cmake .. \
--DPYTHON_INCLUDE_DIR=$(python -c "import sysconfig; print(sysconfig.get_path('include'))")  \
--DPYTHON_LIBRARY=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+-DPYTHON_INCLUDE_DIR=$(python3 -c "import sysconfig; print(sysconfig.get_path('include'))")  \
+-DPYTHON_LIBRARY=$(python3 -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 make
 export PYTHONPATH=$PWD:$PYTHONPATH
+pypath=$(python3 -m site --user-base)
+if ! command -v pybind11-stubgen &> /dev/null
+then
+    # for ghc cluster
+    alias pybind11-stubgen=$pypath/bin/pybind11-stubgen
+fi
 pybind11-stubgen tensorlib -o . --no-setup-py
 mv tensorlib-stubs/__init__.pyi ../tensorlib/__init__.pyi
 rm tensorlib-stubs -rf
