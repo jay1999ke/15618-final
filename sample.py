2e216117abee69e934dbf16dc3dcee8dc8a96920
jay1999ke <jay1999ke@gmail.com>
2023-05-04 11:06:22 -0400

    comment out torch code

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 
-from torch import Tensor as pTensor
+# from torch import Tensor as pTensor
 from autodiff import Tensor
 import numpy as np
 
@@ -11,51 +11,51 @@ nd = np.random.random((1,1))
 ne = np.random.random((1,5))
 
 
-pa = pTensor(na)
-pa.requires_grad = True
+# pa = pTensor(na)
+# pa.requires_grad = True
 a = Tensor(na)
 a.requires_grad = True
 a.gpu()
 
-pb = pTensor(nb)
-pb.requires_grad = True
+# pb = pTensor(nb)
+# pb.requires_grad = True
 b = Tensor(nb)
 b.requires_grad = True
 b.gpu()
 
-pc = pTensor(nc)
-pc.requires_grad = True
+# pc = pTensor(nc)
+# pc.requires_grad = True
 c = Tensor(nc)
 c.requires_grad = True
 c.gpu()
 
-pd = pTensor(nd)
-pd.requires_grad = True
+# pd = pTensor(nd)
+# pd.requires_grad = True
 d = Tensor(nd)
 d.requires_grad = True
 d.gpu()
 
-pe = pTensor(ne)
-pe.requires_grad = True
+# pe = pTensor(ne)
+# pe.requires_grad = True
 e = Tensor(ne)
 e.requires_grad = True
 e.gpu()
 
-pz = (pa + pb) * (pa*pb) + pc + pd + (pc*pd) + pe + (pc/pd) + (pc - pd) * (-pe)
-pz = pz.sum(0, keepdim=True).sum(1, keepdim=True)
+# pz = (pa + pb) * (pa*pb) + pc + pd + (pc*pd) + pe + (pc/pd) + (pc - pd) * (-pe)
+# pz = pz.sum(0, keepdim=True).sum(1, keepdim=True)
 z = (a + b) * (a*b) + c + d + (c*d) + e + (c/d) + (c - d) * (-e)
 z = z.sum(0).sum(1)
 
-print(pz,z)
-pz.backward()
+# print(pz,z)
+# pz.backward()
 
 z.backward()
 
-ps = [pa,pb,pc,pd,pe]
+# ps = [pa,pb,pc,pd,pe]
 s = [a,b,c,d,e]
 
 # compare results: pytorch autograd vs autodiff
-for q,w in zip(ps, s):
+for q,w in zip(s, s):
     print(q.grad)
     print(w.grad)   
     print()
